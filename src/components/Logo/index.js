e92f7bc8f3bea045912f487c1422eed56ae123c7
Riyad0821 <mdriyad0821@gmail.com>
2022-10-14 23:23:14 +0600

    Add basic structure of home page

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import logo from '../../assets/images/logo.png'
+import '../../assets/sass/_logo.scss'
 
 export default function Logo() {
     return (
-        <div>This is Logo</div>
+        <div className='logo'>
+            <img src={logo} alt="Prothom Alo" />
+            <hr />
+        </div>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Logo from '../../components/Logo'
 import Navbar from './Navbar'
+import SelectedNews from './SelectedNews'
+import TrendingNews from './TrendingNews'
 
 export default function Home() {
     return (
         <div>
             <Logo />
             <Navbar />
+            <TrendingNews />
+            <SelectedNews />
         </div>
     )
 }
